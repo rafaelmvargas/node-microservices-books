36567cf1fae47782fa75e71c825c57f0336d886c
Rafael Vargas <rafael.vargas@outlook.com>
2022-04-09 01:57:30 -0400

    re-format, delete .pretterrc file

--- books.js
+++ books.js
@@ -1,24 +1,24 @@
-const express = require("express")
-const app = express()
+'use strict';
 
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/books', () => {
-    console.log('connected to mongodb')
-})
-
+  console.log('connected to mongodb');
+});
 
 app.get('/', (req, res) => {
-    res.send('This is our main endpoint')
-})
-
+  res.send('This is our main endpoint');
+});
 
 // create func
 app.post('/books', (req, res) => {
-    console.log(req.body)
-    res.send('This is our create endpoint')
-})
+  console.log(req.body);
+  res.send('This is our create endpoint');
+});
 
 app.listen(4545, () => {
-    console.log('Book Server is running on port 4545')
-})
+  console.log('Book Server is running on port 4545');
+});
