f129aa56ed420e67eb29fb72fd2351ae78bc3322
Rafael Vargas <rafael.vargas@outlook.com>
2022-04-13 21:41:48 -0400

    create instance of model and post /book route

--- books.js
+++ books.js
@@ -8,6 +8,9 @@ app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 
+require('./book');
+const Book = mongoose.model('Book');
+
 mongoose.connect('mongodb://localhost/books', () => {
   console.log('connected to mongodb');
 });
@@ -18,9 +21,33 @@ app.get('/', (req, res) => {
 
 // create func
 app.post('/book', (req, res) => {
+  let newBook = {
+    title: req.body.title,
+    author: req.body.author,
+    numberPages: req.body.numberPages,
+    publisher: req.body.publisher,
+  };
+
+  // Create a new Book
+  let book = new Book(newBook);
+
+  // Save objects in collection
+  book
+    .save()
+    .then(() => {
+      console.log('New book create!');
+    })
+    .catch((err) => {
+      if (err) {
+        throw err;
+      }
+    });
+
   console.log(req.body);
-  res.send('00:D');
+  res.send('A new book created with success');
 });
+
+
 
 app.listen(4545, () => {
   console.log('Book Server is running on port 4545');
